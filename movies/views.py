dd8700059e2b89cf1a7e135eb073c144e3d702a5
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-07 16:15:36 +0000

    Added context and logic to home page

--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'movies/index.html', {'movie': 'Blade Runner 2049'})
+    context = {
+        'movies': [
+            'Blade Runner 2049',
+            'Glory',
+            'The Matrix',
+            'Millenium Actress'
+        ]
+    }
+    return render(request, 'movies/index.html', context)
 
 def about(request):
     return render(request, 'movies/about.html')
