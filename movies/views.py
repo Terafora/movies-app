416fa994c5948e8e7d13f1851eadce6b05b8edcd
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-07 16:07:35 +0000

    Finished about me challenge and added a variable into index template.

--- movies/views.py
+++ movies/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('My favourite movies')
+    return render(request, 'movies/index.html', {'movie': 'Blade Runner 2049'})
+
+def about(request):
+    return render(request, 'movies/about.html')
